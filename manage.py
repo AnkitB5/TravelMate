631a6b9f50e1a5598ddfe7e8d89e02e3b7b5e084
Akash-Satya <akashsatya13@gmail.com>
2025-04-17 16:24:39 -0400

    added to akash's branch

--- manage.py
+++ manage.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
+"""Django's command-line utility for administrative tasks in akash's branch."""
 import os
 import sys
 
